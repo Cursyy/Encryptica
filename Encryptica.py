f10e7b8ed6b94590bbd522b6bad3f7260f208c19
Cursyy <cursyy.developer@gmail.com>
2024-08-30 19:05:24 +0100

    blowfish encrypting method and random between encrypting methods added

--- Encryptica.py
+++ Encryptica.py
@@ -107,3 +107,74 @@ def des_decrypt(key, ciphertext, iv):
     cipher = DES.new(key, DES.MODE_CBC, iv=iv)
     decrypted_message = unpad(cipher.decrypt(ciphertext), DES.block_size)
     return decrypted_message.decode()  
+
+# Blowfish
+def create_blowfish_cipher(key):
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC)
+    return cipher
+
+def blowfish_encrypt(cipher, plaintext):
+    plaintext = pad(plaintext.encode(), Blowfish.block_size)
+    ciphertext = cipher.encrypt(plaintext)
+    return ciphertext, cipher.iv
+
+def blowfish_decrypt(key, ciphertext, iv):
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv=iv)
+    decrypted_message = unpad(cipher.decrypt(ciphertext), Blowfish.block_size)
+    return decrypted_message.decode() 
+
+# Випадковий вибір шифру
+def randomize_crypt():
+    return random.randint(0, 4)
+
+# Шифрування
+def encrypt(text):
+    encrypted_text = []
+    crypto_pattern = []
+    rsa_keys = {} 
+    for i in range(len(text)):
+        crypto_type = randomize_crypt()
+        crypto_pattern.append(crypto_type)
+        if crypto_type == 0:
+            encrypted_text.append(CaesarEncrypt(text[i], 3))
+        elif crypto_type == 1:
+            private_key, public_key = generate_rsa_keys()
+            rsa_keys[i] = private_key  # Зберігаємо приватний ключ
+            encrypted_text.append((rsa_encrypt(public_key, text[i]), public_key))
+        elif crypto_type == 2:
+            key = b'8bytekey'  # 8 байт для DES
+            cipher = create_des_cipher(key)
+            ciphertext, iv = des_encrypt(cipher, text[i])
+            encrypted_text.append((ciphertext, iv))
+        elif crypto_type == 3:
+            key = b'Sixteen byte key'
+            cipher = create_blowfish_cipher(key)
+            ciphertext, iv = blowfish_encrypt(cipher, text[i])
+            encrypted_text.append((ciphertext, iv))
+        elif crypto_type == 4:
+            encrypted_text.append(vigenere_encrypt(text[i], "key"))
+    return encrypted_text, crypto_pattern, rsa_keys
+
+# Розшифрування
+def decrypt(encrypted_text, crypto_pattern, rsa_keys):
+    decrypted_text = []
+    for i in range(len(encrypted_text)):
+        crypto_type = crypto_pattern[i]
+        if crypto_type == 0:
+            decrypted_text.append(CaesarDecrypt(encrypted_text[i], 3))
+        elif crypto_type == 1:
+            private_key = rsa_keys[i] 
+            decrypted_text.append(rsa_decrypt(private_key, encrypted_text[i][0]))
+        elif crypto_type == 2:
+            key = b'8bytekey'
+            decrypted_text.append(des_decrypt(key, encrypted_text[i][0], encrypted_text[i][1]))
+        elif crypto_type == 3:
+            key = b'Sixteen byte key'
+            decrypted_text.append(blowfish_decrypt(key, encrypted_text[i][0], encrypted_text[i][1]))
+        elif crypto_type == 4:
+            decrypted_text.append(vigenere_decrypt(encrypted_text[i], "key"))
+    return ''.join(decrypted_text)
+
+encrypted_data, pattern, rsa_keys = encrypt(text)
+print("Encrypted:", encrypted_data)
+print("Decrypted:", decrypt(encrypted_data, pattern, rsa_keys))
